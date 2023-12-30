b286ffd09365a640124cac8909ec55f197acd219
Kevyn Barbosa <Kevyn Barbosa>
2023-12-30 15:04:19 -0300

    First commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["nuxt-primevue"],
+  css: [
+    "primevue/resources/themes/lara-light-green/theme.css",
+    "primeicons/primeicons.css",
+    "primeflex/primeflex.min.css",
+  ],
+});
---
+++ pages/teste.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="flex flex-column md:flex-row md:justify-content-between row-gap-3"
+  >
+    <Button type="button" label="Button 1"></Button>
+    <Calendar v-model="date" />
+    <!-- <Button type="button" label="Button 2" severity="secondary"></Button> -->
+    <Button type="button" label="Button 3" severity="help"></Button>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+
+const props = defineProps([]);
+
+const date = ref(null);
+
+onMounted(() => {
+  // Mounted
+});
+</script>
