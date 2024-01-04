31b35adfbda3e7c0f7612326a60ceeb576b98628
Kevyn Barbosa <Kevyn Barbosa>
2024-01-04 11:54:25 -0300

    Estilo de botao

--- pages/teste.vue
+++ pages/teste.vue
@@ -20,16 +20,19 @@ onMounted(() => {
 
 <style scoped>
 :deep(.p-button) {
-  background-image: linear-gradient(135.89deg, #d03355 -5.11%, #fb7a6b 97.89%);
   border-radius: 50px;
   border-style: hidden;
-  /* transition: background 1s ease-out; */
+  background-image: linear-gradient(135.89deg, #d03355, #fb7a6b, #fb7a6b, #d03355);
+  background-size: 200%;
+  background-position: left;
+  transition: all 0.3s ease-in;
 }
 
 :deep(.p-button:hover) {
-  /* background: linear-gradient(135.89deg, #fb7a6b -5.11%, #d03355 97.89%); */
-  background-position: revert;
+  background-position: right;
 }
 
-/* linear-gradient(135.89deg, #D03355 -5.11%, #FB7A6B 97.89%) */
+:deep(.p-button:focus) {
+  box-shadow: none;
+}
 </style>
