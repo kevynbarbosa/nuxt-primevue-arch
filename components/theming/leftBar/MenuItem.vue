261cc736c3544e09f8db21649c784b319a8793f4
Kevyn Barbosa <Kevyn Barbosa>
2024-01-15 20:46:33 -0300

    fix menu

--- components/theming/leftBar/MenuItem.vue
+++ components/theming/leftBar/MenuItem.vue
@@ -1,10 +1,12 @@
 <template>
   <li v-if="item.children == undefined">{{ item.label }}</li>
   <template v-else>
-    <div>{{ item.label }}</div>
-    <ul>
-      <ThemingLeftBarMenuExpand v-for="child in item.children" :key="child.label" :item="child" />
-    </ul>
+    <li>
+      <div>{{ item.label }}</div>
+      <ul>
+        <ThemingLeftBarMenuItem v-for="child in item.children" :key="child.label" :item="child" />
+      </ul>
+    </li>
   </template>
 </template>
 
