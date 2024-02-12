322a85b19556c42fde64145e012c336c8f790ded
Kevyn Barbosa <kevyn@semplice.net.br>
2024-02-12 02:41:15 -0300

    Layout Top Bar

--- components/system/TopBar.vue
+++ components/system/TopBar.vue
@@ -1,10 +1,31 @@
 <template>
   <div
-    class="flex justify-content-between align-items-center px-5 surface-0 border-bottom-1 surface-border relative lg:static"
-    style="height: 60px"
+    class="px-4 fixed flex justify-content-between align-items-center border-bottom-1 surface-border surface-0"
+    style="height: 60px; z-index: 1000; width: -webkit-fill-available"
   >
-    Top Bar
+    <Button icon="pi pi-bars" text rounded />
+
+    <!-- <IconField iconPosition="left">
+      <InputIcon>
+        <i class="pi pi-search" />
+      </InputIcon>
+      <InputText v-model="pesquisar" placeholder="Pesquisar" />
+    </IconField> -->
+
+    <IconField>
+      <InputIcon>
+        <i class="pi pi-spin pi-spinner" />
+      </InputIcon>
+      <InputText v-model="pesquisar" />
+    </IconField>
+
+    <div>
+      <Button icon="pi pi-bell" text rounded />
+      <Button icon="pi pi-sign-out" text rounded />
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const pesquisar = ref("");
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="min-h-screen flex relative surface-ground">
+  <div class="min-h-screen flex surface-ground">
     <SystemLeftBar :size="leftBarSize" :topBarSize="topBarSize"></SystemLeftBar>
 
-    <div class="min-h-screen flex flex-column relative flex-auto">
-      <div class="fixed w-full" style="z-index: 1000">
-        <SystemTopBar></SystemTopBar>
-      </div>
+    <div class="min-h-screen flex flex-column flex-auto">
+      <SystemTopBar></SystemTopBar>
 
       <div class="h-full bg-[#edf1f6]" :style="{ 'margin-top': topBarSize + 'px' }">
         <div class="h-full p-2 flex flex-column flex-auto">
